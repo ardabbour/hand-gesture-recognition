21512f3acb447bb5070c36cbee3b0abd3e214622
Abdul Rahman Dabbour <thedabbour@gmail.com>
2017-05-29 13:51:48 +0300

    Update

--- hand_gesture_recognition.py
+++ hand_gesture_recognition.py
@@ -5,21 +5,43 @@
     Cognitive Robotics Laboratory
 
     General Hand Gesture Recoginition using OpenCV
-    Details: https://github.com/thedabbour/hand-gesture-recognition/README.md
+    Details: https://github.com/thedabbour/hand-gesture-recognition/README.md/
 """
 
 import cv2
 import numpy as np
 
+# Create a flag to indicate when the application should start working
 FLAG = False
 
+# Create camera source as webcam
 CAP = cv2.VideoCapture(0)
-TRACKER = cv2.Tracker_create('KCF')
 
-# Try this for now
+# Read first frame.
+OK, FRAME = CAP.read()
+
+# Define an initial region of interest (ROI) based on frame size
+HEIGHT, WIDTH = FRAME.shape[:2]
+HEIGHT = float(HEIGHT)
+WIDTH = float(WIDTH)
+
+X = int(((2*WIDTH)/3.0) - ((WIDTH/3.0)*0.5))
+Y = int((HEIGHT/3.0) - ((HEIGHT/3.0)*0.5))
+W = int((WIDTH/3.0))
+H = int(1.5*W)
+
+ROI = (X, Y, W, H)
+
+
+# Create and initialize Kernalized Correlation Filters tracker around ROI
+# More information on KCF object tracker: http://www.robots.ox.ac.uk/~joao/circulant/
+TRACKER = cv2.Tracker_create('KCF')
+OK = TRACKER.init(FRAME, ROI)
+
+# Define approximate hue, saturation and value range for skin color
+# HSV values for skin taken from http://pyimagesearch.com/
 LOWER = np.array([0, 48, 80], dtype="uint8")
 UPPER = np.array([20, 255, 255], dtype="uint8")
-################################################
 
 while True:
     # Capture camera input frame-by-frame
@@ -29,8 +51,66 @@ while True:
         # Set frame to mirror mode
         FRAME = cv2.flip(FRAME, 1)
 
-        # Define a region of interest (ROI)
-        if not FLAG:
+        # Begin hand gesture recognition
+        if FLAG:
+            # Convert camera colorspace to HSV
+            CONVERTED = cv2.cvtColor(FRAME, cv2.COLOR_BGR2HSV)
+
+            # Filter image to show HSV values of skin only
+            SKINMASK = cv2.inRange(CONVERTED, LOWER, UPPER)
+
+            # Create structuring element for morphological operations
+            KERNEL = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+
+            # Erode and dilate to remove small misidentified blobs
+            SKINMASK = cv2.erode(SKINMASK, KERNEL, iterations=2)
+            SKINMASK = cv2.dilate(SKINMASK, KERNEL, iterations=2)
+
+            # Filter with Gaussian to smooth and remove noise
+            SKINMASK = cv2.GaussianBlur(SKINMASK, (3, 3), 0)
+
+            # Show only areas of original image with skin
+            SKIN = cv2.bitwise_and(FRAME, FRAME, mask=SKINMASK)
+
+            OK, ROI_TRACKER = TRACKER.update(FRAME)
+            if OK:
+                X = int(ROI_TRACKER[0])
+                Y = int(ROI_TRACKER[1])
+                W = int(ROI_TRACKER[2])
+                H = int(ROI_TRACKER[3])
+
+                W1 = X
+                W2 = X + W
+                H1 = Y
+                H2 = Y + H
+
+            # Create mask to remove everything but ROI from image
+            AREAMASK = np.zeros(SKIN.shape, np.uint8)
+            AREAMASK[Y:Y + H, X:X + W] = SKIN[Y:Y + H, X:X + W]
+
+            # Apply Canny edge detector to the mask
+            CANNY = cv2.Canny(AREAMASK, 100, 200)
+
+            IM2, CONTRS, HRRCHY = cv2.findContours(CANNY, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            KERNEL = np.ones((5, 5), np.uint8)
+
+            MORPH = cv2.erode(CANNY, KERNEL, iterations=2)
+            MORPH = cv2.dilate(CANNY, KERNEL, iterations=2)
+
+            cv2.drawContours(FRAME, CONTRS, -1, (0, 255, 0), 3)
+
+            # Draw a rectangle around the ROI
+            cv2.rectangle(FRAME, (W1, H1), (W2, H2), (255, 0, 0), 2)
+
+            # For diagnostics, show different outputs
+            # cv2.imshow('Canny Output', CANNY)
+            # cv2.imshow('Area-Masked Output', AREAMASK)
+            # cv2.imshow('Color-Masked Output', SKIN)
+
+        # Prepare for start/Reset
+        else:
+            # Define a region of interest (ROI)
             HEIGHT, WIDTH = FRAME.shape[:2]
             HEIGHT = float(HEIGHT)
             WIDTH = float(WIDTH)
@@ -40,91 +120,31 @@ while True:
             W = int((WIDTH/3.0))
             H = int(1.5*W)
 
-        if FLAG:
-            OK, ROI_TRACKER = TRACKER.update(FRAME)
-            if OK:
-                X = int(ROI_TRACKER[0])
-                Y = int(ROI_TRACKER[1])
-                W = int(ROI_TRACKER[2])
-                H = int(ROI_TRACKER[3])
+            W1 = X
+            W2 = X + W
+            H1 = Y
+            H2 = Y + H
 
-        # Color-based filtering
-        CONVERTED = cv2.cvtColor(FRAME, cv2.COLOR_BGR2HSV)
-        SKINMASK = cv2.inRange(CONVERTED, LOWER, UPPER)
+            # Draw a rectangle around the ROI
+            cv2.rectangle(FRAME, (W1, H1), (W2, H2), (255, 0, 0), 2)
 
-        KERNEL = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-        SKINMASK = cv2.erode(SKINMASK, KERNEL, iterations=2)
-        SKINMASK = cv2.dilate(SKINMASK, KERNEL, iterations=2)
-
-        SKINMASK = cv2.GaussianBlur(SKINMASK, (3, 3), 0)
-        SKIN = cv2.bitwise_and(FRAME, FRAME, mask=SKINMASK)
-
-        W1 = X
-        W2 = X + W
-        H1 = Y
-        H2 = Y + H
-
-        ROI = FRAME[H1:H2, W1:W2]
-
-        # Create a mask that covers everything but the ROI
-        MASK = np.zeros(FRAME.shape, np.uint8)
-        MASK[H1:H2, W1:W2] = FRAME[H1:H2, W1:W2]
-
-        # Apply Canny edge detector to the mask
-        CANNY = cv2.Canny(MASK, 100, 200)
-
-        cv2.rectangle(FRAME, (W1, H1), (W2, H2), (255, 0, 0), 2)
-
-        IM2, CONTOURS, HIERARCHY = cv2.findContours(CANNY, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        KERNEL = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-        KERNEL_MOD = np.ones((5, 5), np.uint8)
-        MORPH = cv2.erode(CANNY, KERNEL, iterations=2)
-        MORPH = cv2.dilate(CANNY, KERNEL, iterations=2)
-
-        MORPH_MOD = cv2.erode(CANNY, KERNEL_MOD, iterations=2)
-        MORPH_MOD = cv2.dilate(CANNY, KERNEL_MOD, iterations=2)
-
-        cv2.drawContours(FRAME, CONTOURS, -1, (0, 255, 0), 3)
-
-        # Display the resulting frames
-        cv2.imshow('Camera Output', FRAME)
-        # cv2.imshow('Canny Edge Detection', CANNY)
-        cv2.imshow('Dilated and Eroded Canny', MORPH)
-        cv2.imshow('Dilated and Eroded Canny with simple', MORPH_MOD)
-        # cv2.imshow("Skin Filter", SKIN)
-        # cv2.imshow('Contours', IM2)
-        # cv2.imshow("images", np.hstack([FRAME, MORPH, MORPH_MOD]))
+        # Display the original image with tracking
+        cv2.imshow('Camera Input', FRAME)
 
     INTERRUPT = cv2.waitKey(1)
 
+    # Press 's' to start hand gesture recognition
     if INTERRUPT & 0xFF == ord('s'):
-        TRACKER.init(FRAME, (X, Y, W, H))
         FLAG = True
 
-    if INTERRUPT & 0xFF == ord('f'):
+    # Press 'e' to end hand gesture recognition
+    if INTERRUPT & 0xFF == ord('e'):
         FLAG = False
 
+    # Press 'q' to quit the program
     if INTERRUPT & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# When everything is done, release the camera and close all windows
 CAP.release()
 cv2.destroyAllWindows()
-
-
-# import cv2
-# import numpy as np
-
-# img = cv2.imread('input.png', 0)
-
-# kernel = np.ones((5,5), np.uint8)
-
-# img_erosion = cv2.erode(img, kernel, iterations=1)
-# img_dilation = cv2.dilate(img, kernel, iterations=1)
-
-# cv2.imshow('Input', img)
-# cv2.imshow('Erosion', img_erosion)
-# cv2.imshow('Dilation', img_dilation)
-
-# cv2.waitKey(0)
